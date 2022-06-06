790e726c6da5bfcfe3d93982665e47c1f8a9d4bf
Vera Ho <vh71886@gmail.com>
2022-06-06 15:08:26 -0400

    Added alt key logic

--- minesweeper_react/components/tile.jsx
+++ minesweeper_react/components/tile.jsx
@@ -8,44 +8,34 @@ export default class Tile extends React.Component {
         super(props)
         this.state = {
             status: "tile",
-            text: "T"
+            text: ""
         }
         this.handleClick = this.handleClick.bind(this)
-       
-        // console.log(this.props.tile.bombed)
-        // this.test = (<div className="tile" onClick={this.handleClick}>
-        //     T
-        // </div>)
     }
 
 
 
-    handleClick() {
+    handleClick(e) {
+        e.preventDefault();
+        console.log(e);
+
         let bombCount = this.props.tile.adjacentBombCount() 
         bombCount === 0 ? bombCount = null : bombCount
 
-        if (!this.props.tile.explored && !this.props.tile.bombed) {
+        if (!this.props.tile.explored && !this.props.tile.bombed && !e.altKey) {
             console.log("revealed")
             this.setState({ status: "revealed", text: bombCount })
-            // this.test = (
-            //         <div className="revealed">{bombCount}</div>
-            //     )
+
         } else if (this.props.tile.bombed){
             console.log("bombed")
             this.setState({ status: "bombed", text: "B" })
-            // this.test = (
-            //     <div className="bombed">B</div>
-            // )
-        } else if (this.props.tile.flagged){
+
+        } else if (e.altKey){
             console.log("flagged")
             this.setState({ status: "flagged", text: "F" })
-            // this.test = (
-            //     <div className="flagged">F</div>
-            // )
+
         } 
-        console.log(bombCount)
         this.props.update();
-        
     }
 
     render() {
