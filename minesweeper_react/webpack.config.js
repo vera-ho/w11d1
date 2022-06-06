94fff59d7708e9584c6d7a96a7a1a6102cb3f3ae
Vera Ho <vh71886@gmail.com>
2022-06-06 10:27:16 -0400

    Initial commit

---
+++ minesweeper_react/webpack.config.js
@@ -0,0 +1,29 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: "./react_minesweeper.jsx",
+  output: {
+      path: path.resolve(__dirname),
+      filename: "bundle.js"
+  },
+  devtool: 'source-map',
+  resolve: {
+      extensions: ['.js', '.jsx', '*']
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env', '@babel/react']
+          }
+        },
+      }
+    ]
+  }
+};
