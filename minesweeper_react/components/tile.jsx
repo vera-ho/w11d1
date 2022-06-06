f54e6b7575edc7808d4cda960bf88105568fb646
Vera Ho <vh71886@gmail.com>
2022-06-06 17:00:16 -0400

    Updating tile logic

--- minesweeper_react/components/tile.jsx
+++ minesweeper_react/components/tile.jsx
@@ -18,35 +18,56 @@ export default class Tile extends React.Component {
         e.preventDefault();
         let flagged = e.altKey;
 
-        let bombCount = this.props.tile.adjacentBombCount() 
-        bombCount === 0 ? bombCount = null : bombCount
-
-        if (!this.props.tile.explored && !this.props.tile.bombed && !e.altKey) {
-            this.setState({ status: "revealed", text: bombCount })
-        } else if (this.props.tile.bombed){
-            this.setState({ status: "bombed", text: "B" })
-        } else if (e.altKey){
-            if(!this.props.tile.flagged) {
-                this.setState({ status: "flagged", text: "F" })
-            } else {
-                this.setState({ status: "tile", text: "" })
-            }
-        } 
+        
+        // if (e.altKey){
+        //     if(!this.props.tile.flagged) {
+        //         this.setState({ status: "flagged", text: "F" })
+        //     } else {
+        //         this.setState({ status: "tile", text: "" })
+        //     }
+        // }
         this.props.update(this.props.tile, flagged);
     }
 
     checkTileState() {
-        if(this.props.tile.explored) {
-            this.setState({ status: "revealed", text: "" })
-        }
     }
 
     render() {
-        this.checkTileState();
+        // this.checkTileState();
+        let type = "";
+        let text = "";
+        let bombCount = this.props.tile.adjacentBombCount() ;
+        bombCount === 0 ? bombCount = null : bombCount;
+      
+        // debugger
+        if (!this.props.tile.explored) {
+            type = "tile";
+            text = "";
+        } else 
+        if (this.props.tile.explored && !this.props.tile.bombed) {
+            type = "revealed";
+            text = bombCount;
+            // this.setState({ status: "revealed", text: bombCount })
+        } else if (this.props.tile.bombed){
+            type = "bombed";
+            text = "B";
+            // this.setState({ status: "bombed", text: "B" })
+        } else if(this.props.tile.flagged) {
+            type = "flagged";
+            text = "F";
+        } else {
+            type = "revealed";
+            text = "";
+        }
+        // } else if(this.props.tile.explored && !this.props.tile.bombed) {
+        //     type = "revealed";
+        //     text = "";
+        //     // this.setState({ status: "revealed", text: "" })
+        // }
 
-        return(
-            (<div className={this.state.status} onClick={this.handleClick}>
-            {this.state.text}
+        return (
+            (<div className={type} onClick={this.handleClick}>
+            {text}
         </div>)
         )
 
